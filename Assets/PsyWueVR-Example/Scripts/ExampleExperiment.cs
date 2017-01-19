5eea4c23270464a16d62711cefbcea1a618e85ed
Nico Balbach <nico.balbach@stud-mail.uni-wuerzburg.de>
2017-01-19 18:58:30 +0100

    Added targets and experiment segments

--- Assets/PsyWueVR-Example/Scripts/ExampleExperiment.cs
+++ Assets/PsyWueVR-Example/Scripts/ExampleExperiment.cs
@@ -7,83 +7,115 @@ public class ExampleExperiment : Experiment
 {
 
 	public GUIRate guiRate;
+	public GameObject start;
+	public GameObject target1;
+	public GameObject target2;
 
 	public override void initValues ()
 	{
-		name = "Example Experiment";
+		name = "Balloon Experiment";
 		input = "input.txt";
 	}
 
 	public override void init ()
 	{
-		//A function can be made with Lamda function ( ()=>{} ) or by calling a void function 
+		status = "Wellcome";
+		instruction = "Wellcome! You wanna learn how hot air balloons navigate?\n Then please press <space> to begin!";
+		target1.SetActive (false);
+		target2.SetActive (false);
 
-		//Functions/Actions made with Lamda
-		/*phases.Add (new ExperimentPhase (5, ()=>{
-			status = "Possibilities";
-			instruction = "You can iterate your own methods with next. Execute everything in next or do everything else you please.";
-		}));*/
+		phases.Add (new ExperimentPhase (5, ()=>{
+			status = "";
+			instruction = "Hot air balloon navigate by turning a hot source under the balloon on and off.";
+		}));
 
-		//Functions/Actions made with void Methods
-		phases.Add (new ExperimentPhase (1, walkToRead));
-		phases.Add (new ExperimentPhase (1, rate));
-		phases.Add (new ExperimentPhase (5, end));
+		phases.Add (new ExperimentPhase (5, ()=>{
+			instruction = "Turning a hot source like fire on will make the balloon rise.\nTurning it off will make it lower.";
+		}));
 
-		status = "Instructions";
-		instruction = "Instructions can be declarated here. Please press <Space> to continue.";
-	}
+		phases.Add (new ExperimentPhase (1, ()=>{
+			isBlackout = false;
+			subject.representation.transform.position = start.transform.position;
+			instruction = "";
+		}));
 
-	private void walkToRead()
-	{
-		subject.representation.GetComponent<Movement> ().canMove = true;
-		isBlackout = false;
-		status = "";
-		instruction = "";
-		pause = true;
-	}
+		phases.Add (new ExperimentPhase (2, ()=>{
+			instruction = "This is our playground!";
+		}));
 
-	private void guiDialog ()
-	{	
-		subject.representation.GetComponent<Movement> ().canMove = false;
-		isBlackout = false;
-		status = "GUI Dialog";
-		instruction = "Different gui elements can be added. For example a rating gui like the following.";
-	}
+		phases.Add (new ExperimentPhase (5, ()=>{
+			instruction = "You can control this balloon by pressing <space> to turn the fire on and releasing <space> to turn it off.";
+		}));
 
-	private void rate ()
-	{
-		instruction = "";
-		status = "";
-		isBlackout = false;
+		phases.Add (new ExperimentPhase (3, ()=>{
+			instruction = "Let's go! Try it out!";
+		}));
 
-		guiRate.Show ();
-		guiRate.setHigh ("1 Heigh");
-		guiRate.setLow ("0 Low");
-		guiRate.setInstructions ("Please rate with <a>/<left> and <d>/<right>. Submit with <space>.");
-		guiRate.setStartValue (0.5f);
-		guiRate.StartRating ();
+		phases.Add (new ExperimentPhase (3, ()=>{
+			instruction = "Press and release <space> to control the flame";
+			subject.representation.GetComponent<Movement> ().canMove = true;
+		}));
 
-		StartCoroutine ("ratePause");
-	}
+		phases.Add (new ExperimentPhase (8, ()=>{
+			instruction = "";
+		}));
 
-	private IEnumerator ratePause ()
-	{
-		pause = true;
-		//pause until button pressed
-		while (!Input.GetButtonDown ("Submit")) {
-			//Wait until key pressed
-			yield return null;
-		}
-		float rating = guiRate.EndRating ();
-		writer.writeLine ("Enjoyment:" + rating);
-		guiRate.Hide ();
-		pause = false;
+		phases.Add (new ExperimentPhase (5, ()=>{
+			isBlackout = true;
+			instruction = "The balloons will be pushed in different directions on different heights.";
+			subject.representation.GetComponent<Movement> ().canMove = false;
+		}));
 
-		return false;
-	}
+		phases.Add (new ExperimentPhase (7, ()=>{
+			instruction = "This is caused by the different wind directions and it's the way hot air balloons navigate.";
+		}));
 
-	private void end()
-	{
-		instruction = "Thank you for your patience!";
+		phases.Add (new ExperimentPhase (4, ()=>{
+			instruction = "Now you know how hot air balloons navigate!";
+		}));
+
+		phases.Add (new ExperimentPhase (1, ()=>{
+			subject.representation.transform.position = start.transform.position;
+			isBlackout = false;
+			instruction = "";
+		}));
+
+		phases.Add (new ExperimentPhase (5, ()=>{
+			instruction = "Try and navigate to the red spot!";
+			subject.representation.GetComponent<Movement> ().canMove = true;
+			target1.SetActive (true);
+		}));
+			
+		phases.Add (new ExperimentPhase (1, ()=>{
+			instruction = "";
+			pause = true;
+		}));
+
+		phases.Add (new ExperimentPhase (3, ()=>{
+			instruction = "You made it!";
+			target1.SetActive (false);
+		}));
+			
+		phases.Add (new ExperimentPhase (1, ()=>{
+			instruction = "";
+			target2.SetActive (true);
+		}));
+
+		phases.Add (new ExperimentPhase (5, ()=>{
+			instruction = "Now try and navigate to the green spot!";
+		}));
+
+		phases.Add (new ExperimentPhase (1, ()=>{
+			instruction = "";
+			pause = true;
+		}));
+
+		phases.Add (new ExperimentPhase (3, ()=>{
+			instruction = "Perfect!";
+		}));
+
+		phases.Add (new ExperimentPhase (5, ()=>{
+			instruction = "Well done and thank you for your patience!\nWe hope you liked this experiment!";
+		}));
 	}
 }
